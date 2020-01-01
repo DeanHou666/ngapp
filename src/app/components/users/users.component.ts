62575b395f5d3145accb3abd10d4680cfdbacade
Dean <Diamond.hou@hotmail.com>
2020-01-01 12:34:40 +0800

    start files

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  firstName= 'John';
+  lastName= 'doe';
+  age=30;
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+
+  hasBirthday(){
+    this.age +=1;
+  }
+
+}
